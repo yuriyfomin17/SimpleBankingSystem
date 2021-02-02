746fcb1e77980c87f176dbfc58ea7b4e2cd05783
yuriyfomin17 <fominyura2012@gmail.com>
2021-02-02 20:19:52 +0200

    Added SQL

--- Simple Banking System/task/banking/banking.py
+++ Simple Banking System/task/banking/banking.py
@@ -3,25 +3,43 @@ import random
 import sqlite3
 
 
-def luhn(number):
-    luhn_num = []
-    number = str(number)
-    for index in range(len(number)):
-        if (index + 1) % 2 != 0:
-            luhn_num.append(int(number[index]) * 2)
-        else:
-            luhn_num.append(int(number[index]))
-    for index in range(len(luhn_num)):
-        if int(luhn_num[index]) > 9:
-            luhn_num[index] = luhn_num[index] - 9
-    sum_luhn = 0
-    for index in range(len(luhn_num)):
-        sum_luhn += int(luhn_num[index])
+def luhn_checksum_calc(part_code):
+    part_code = part_code + "0"
+
+    def digits_of(n):
+        return [int(d) for d in str(n)]
+
+    digits = digits_of(part_code)
+    odd_digits = digits[-1::-2]
+    even_digits = digits[-2::-2]
+    checksum = 0
+    checksum += sum(odd_digits)
+    for d in even_digits:
+        checksum += sum(digits_of(d * 2))
     count = 0
-    while sum_luhn % 10 != 0:
-        sum_luhn += 1
+    while checksum % 10 != 0:
         count += 1
-    return count
+        checksum += 1
+    result = part_code[0:len(part_code) - 1] + str(count)
+    return result
+
+
+def luhn_checksum(card_number):
+    def digits_of(n):
+        return [int(d) for d in str(n)]
+
+    digits = digits_of(card_number)
+    odd_digits = digits[-1::-2]
+    even_digits = digits[-2::-2]
+    checksum = 0
+    checksum += sum(odd_digits)
+    for d in even_digits:
+        checksum += sum(digits_of(d * 2))
+    return checksum % 10
+
+
+def is_luhn_valid(card_number):
+    return luhn_checksum(card_number) == 0
 
 
 class DBMS:
@@ -48,7 +66,7 @@ class DBMS:
         # After doing some changes in DB don't forget to commit them!
         self.conn.commit()
 
-    def get_account(self, current_acc, current_pin):
+    def get_account(self, current_acc):
         find_acc_command = """SELECT 
             number,
             pin,
@@ -58,8 +76,7 @@ class DBMS:
         WHERE 
             id = {0}
             AND number = {0}
-            AND pin = {1}
-        """.format(current_acc, current_pin)
+        """.format(current_acc)
         self.cur.execute(find_acc_command)
         # Returns all rows from the response
         arr = self.cur.fetchall()
@@ -92,15 +109,14 @@ class Bank:
     def generate_account(self, db_manager):
         iin = 400000
         account_identifier = random.randrange(100000000, 999999999)
-        current_account = "" + str(iin) + str(account_identifier)
-        check_sum = luhn(current_account)
-        current_account += str(check_sum)
+        part_code = "" + str(iin) + str(account_identifier)
+        current_account = int(luhn_checksum_calc(part_code))
         self.pin = random.randrange(1000, 9999)
-        while len(db_manager.get_account(current_account, self.pin)) != 0:
+        while len(db_manager.get_account(current_account)) != 0:
             iin = 400000
             account_identifier = random.randrange(100000000, 999999999)
-            check_sum = luhn(current_account)
-            current_account = "" + str(iin) + str(account_identifier) + str(check_sum)
+            part_code = "" + str(iin) + str(account_identifier)
+            current_account = int(luhn_checksum_calc(part_code))
         self.account = current_account
         db_manager.create_account(self.account, self.pin, 0)
         return [self.account, self.pin]
@@ -122,6 +138,8 @@ for i in range(1, len(menu)):
     print(menu[i])
 print(menu[0])
 option = int(input())
+if option > 2 or option < 0:
+    option = 0
 login_menu = ["0. Exit", "1. Balance", "2. Add income", "3. Do transfer", "4. Close account", "5. Log out"]
 acc = ''
 curr_balance = None
@@ -134,52 +152,57 @@ while menu[option] != "0. Exit":
     elif menu[option] == "2. Log into account":
         print("Enter your card number:")
         acc = input()
-        curr_check_sum = acc[len(acc) - 1]
-        curr_luhn_acc = acc[0:len(acc) - 2]
-        if luhn(int(curr_luhn_acc)) == int(curr_check_sum):
-            print("Enter your PIN:")
-            pin = int(input())
-            curr_arr = database_manager.get_account(acc, pin)
-            if len(curr_arr) == 0:
-                print("Wrong card number or PIN!")
-            else:
-                print("You have successfully logged in!")
-                curr_balance = curr_arr[0][2]
-                curr_pin = curr_arr[0][1]
-                curr_account = curr_arr[0][0]
-                menu = login_menu
+        print("Enter your PIN:")
+        pin = int(input())
+        curr_arr = database_manager.get_account(acc)
+        if len(curr_arr) == 0 or pin != int(curr_arr[0][1]):
+            print("Wrong card number or PIN!")
         else:
-            print("Probably you made a mistake in the card number.")
+            print("You have successfully logged in!")
+            curr_balance = curr_arr[0][2]
+            curr_pin = curr_arr[0][1]
+            curr_account = curr_arr[0][0]
+            menu = login_menu
+
     elif menu[option] == "1. Balance":
-        curr_arr = database_manager.get_account(curr_account, curr_pin)
+        curr_arr = database_manager.get_account(curr_account)
         curr_balance = curr_arr[0][2]
         print("Balance:", curr_balance)
     elif menu[option] == "2. Add income":
         print("Enter income:")
         income = int(input())
-
         b.add_income(database_manager, curr_account, income, curr_balance)
+        curr_arr = database_manager.get_account(curr_account)
+        curr_balance = curr_arr[0][2]
+        curr_pin = curr_arr[0][1]
+        curr_account = curr_arr[0][0]
     elif menu[option] == "3. Do transfer":
-        print("Enter your card number:")
+        print("Enter card number:")
         transfer_acc = input()
-        print("Enter your PIN:")
-        transfer_pin = int(input())
-        curr_arr = database_manager.get_account(transfer_acc, transfer_pin)
-        if len(curr_arr) == 0:
-            print("Wrong card number or PIN!")
-        else:
-            transfer_balance = curr_arr[0][2]
-            transfer_acc = curr_arr[0][1]
-            if transfer_acc != curr_account:
-                print("Enter how much money you want to transfer:")
-                diff = int(input())
-                if diff <= curr_balance:
-                    b.add_income(database_manager, transfer_acc, diff, transfer_balance)
-                    b.add_income(database_manager, curr_account, -diff, curr_balance)
-                else:
-                    print("Not enough money!")
+        if is_luhn_valid(transfer_acc):
+            transfer_arr = database_manager.get_account(transfer_acc)
+            if len(transfer_arr) == 0:
+                print("Such a card does not exist.")
             else:
-                print("You can't transfer money to the same account!")
+                transfer_balance = transfer_arr[0][2]
+                transfer_acc = transfer_arr[0][0]
+                if transfer_acc != curr_account:
+                    print("Enter how much money you want to transfer:")
+                    curr_arr = database_manager.get_account(curr_account)
+                    curr_balance = curr_arr[0][2]
+                    curr_pin = curr_arr[0][1]
+                    curr_account = curr_arr[0][0]
+                    diff = int(input())
+                    if diff <= curr_balance:
+                        print("Transfer account", transfer_acc)
+                        b.add_income(database_manager, transfer_acc, diff, transfer_balance)
+                        b.add_income(database_manager, curr_account, -diff, curr_balance)
+                    else:
+                        print("Not enough money!")
+                else:
+                    print("You can't transfer money to the same account!")
+        else:
+            print("Probably you made a mistake in the card number. Please try again!")
     elif menu[option] == "4. Close account":
         b.delete_acc(database_manager, curr_account)
     elif menu[option] == "5. Log out":
@@ -190,4 +213,6 @@ while menu[option] != "0. Exit":
         print(menu[i])
     print(menu[0])
     option = int(input())
+    if option < 0 or (option > 5 and len(menu) == 6) or (option > 2 and len(menu) == 3):
+        option = 0
 print('Bye!')
